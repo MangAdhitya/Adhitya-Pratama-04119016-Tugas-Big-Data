d5dd70aaf6abfd08bb7745c68d0305d8ae952dca
MangAdhitya <adhityapratama2106@gmail.com>
2021-04-15 17:00:43 +0700

    Tugas P6

---
+++ Hello.py
@@ -0,0 +1,13 @@
+print ("======================================")
+print ("               Biodata    ")
+print ("======================================")
+
+print("Nama     : Adhitya Pratama")
+print("Nim      : 04119016")
+print("Prodi    : Sistem komputer")
+print("Kampus   : Universitas Narotama")
+
+
+print ("======================================")
+print ("               Terimakasih    ")
+print ("======================================")
